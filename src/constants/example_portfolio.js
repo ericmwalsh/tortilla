90ccec471d400ac897ab540dc71bb9a6f7bb00bb
Eric Walsh <ericmatthewwalsh@gmail.com>
2018-01-11 00:00:53 -0800

    add more coins to the example_portfolio, make the default landing a little more colorful

--- src/constants/example_portfolio.js
+++ src/constants/example_portfolio.js
@@ -1,7 +1,7 @@
 const ExamplePortfolio = {
   data0: [
     ['LTC', 5.0],
-    ['NEO', 15.0],
+    ['NEO', 10.0],
     ['XRP', 250.0],
     ['MIOTA', 100.0],
     ['DASH', 1.0],
@@ -11,7 +11,18 @@ const ExamplePortfolio = {
     ['ADA', 500.0],
     ['REQ', 250.0],
     ['QSP', 250.0],
-    ['OMG', 30.0]
+    ['OMG', 30.0],
+    ['XLM', 200.0],
+    ['VEN', 40.0],
+    ['XRB', 20.0],
+    ['LSK', 20.0],
+    ['ZEC', 1.0],
+    ['FCT', 5.0],
+    ['ARK', 20.0],
+    ['STRAT', 15.0],
+    ['XMR', 5.0],
+    ['VTC', 20.0],
+    ['SALT', 20.0]
   ]
 }
 
